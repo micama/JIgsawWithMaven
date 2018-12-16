2e8ce4337aac144a4d7791e38209b718d978a25e
Miguel Martins <miguelcardosomartins@gmail.com>
2018-12-16 16:50:20 +0100

    added third-party dependency

--- jpms.main/src/main/java/module-info.java
+++ jpms.main/src/main/java/module-info.java
@@ -2,8 +2,7 @@
 module jpms.main {
     requires jpms.maths;
     requires jpms.api;
+    requires j2html;
 
     uses org.jpms.api.Greeter;
-
-    //exports org.jpms.main;
 }
--- jpms.main/src/main/java/org/jpms/main/Main.java
+++ jpms.main/src/main/java/org/jpms/main/Main.java
@@ -1,23 +1,32 @@
 package org.jpms.main;
 
-import java.util.ServiceLoader;
+import j2html.TagCreator;
 import org.jpms.api.Greeter;
 import org.jpms.maths.MathUtils;
+
+import java.util.ServiceLoader;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Simple dependency");
         long res = MathUtils.add(1, 2);
         System.out.println("result: " + res);
 
         System.out.println("--------------------");
 
+        System.out.println("Services:");
         ServiceLoader.load(Greeter.class)
-                .stream()
-                .forEach(provider -> provider.get().greet());
+            .stream()
+            .forEach(provider -> provider.get().greet());
 
         System.out.println("--------------------");
+        
+        System.out.println("third-party library:");
+        System.out.println(TagCreator.body().toString());
+        
     }
 
 }
+
